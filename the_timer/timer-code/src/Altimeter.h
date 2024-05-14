0c051fc26aa1700ce20b2119fdb9fdab3917cca9
William Layne <93668014+WilliamLayne@users.noreply.github.com>
2024-05-14 00:29:01 -0600

    Progress on altimeter implementation

--- the_timer/timer-code/src/Altimeter.h
+++ the_timer/timer-code/src/Altimeter.h
@@ -4,13 +4,26 @@
 #include <Arduino.h>
 #include <Wire.h>
 
+//THE PROCEDURE
+    /**
+     * (on startup only) read calibration data and store it
+     * 
+     * from then on any time we want data, 
+     * read D1 (digital pressure), D2 (digital temperature)
+     * calculate temperature: dT and TEMP
+     * calculate temperature compensated pressure: OFF, SENS, and P
+     * 
+     * next, use these values in whatever they need to be used by
+     */
+// We want a total time to do the entire calculation to be < 10ms. We'll need to read the ADC twice and conversion time at OSR 2048 is 4.54ms max which should work?
+
 // Datasheet: 
 // Address is 111011Cx, where C is the complementary value of the pin CSB [(LOW), and x is any value (?)] Therefore, addr = 11101100 = 0x76
 #define ADDRESS 0x76    
 
 #define CMD_RESET       0x1E
 #define CMD_ADC_READ    0x00
-#define CMD_PROM_READ   0xA0
+#define CMD_PROM_READ   0xA0    // Starting prom address. Each subsequent address is 2 bits after the last.
 #define CMD_CONVERT_D1  0x46    // OSR of 2048
 #define CMD_CONVERT_D2  0x56    // OSR of 2048
 
@@ -21,13 +34,44 @@ public:
     float getAltitude();
     float getVelocity();
     float getAvgVelocity();
-    //float getAcceleration();
     void setZero();
 
+    
+
 private:
+    uint32_t m_calculateAltitude();
+
     float rollingAverage(const float &newVel);
 
-    //uint8_t address;
+    //TODO: why doesnt the fucking doxygen newline work
+    /**
+     * @brief Factory calibration coefficients. \n 
+     * | C1 | Pressure sensitivity                  | SENS_T1   | \n 
+     * | C2 | Pressure offset                       | OFF_T1    | \n 
+     * | C3 | Temp. coeff. of pressure sensitivity  | TCS       | \n 
+     * | C4 | Temp. coeff. of pressure offset       | TCO       | \n 
+     * | C5 | Reference temperature                 | T_REF     | \n 
+     * | C6 | Temp. coeff. of the temperature       | TEMPSENS  | \n 
+     */
+    uint16_t m_coefficients[6];
+
+    uint32_t m_D1;    // Digital pressure. may rename
+    uint32_t m_D2;    // Digital temperature. may rename
+
+    // TODO -----------------
+    // these values and the three below are used for doing math and are derived from the constants along with D1 and D2.
+    // these should probably be moved to a function and be declared inside the call...? 
+    // basically, these aren't technically necessary (mostly) and we want to save on as much ram as possible here
+    int32_t m_dT;
+    int32_t m_TEMP;
+
+    // Jesus, int64s 
+    int64_t m_OFF;
+    int64_t m_SENS;
+    int32_t m_P;
+
+    //TODO: does wire even need to be a declared variable.? it can be used statically right but why doesn't the library??
+    TwoWire *m_i2c;
 
     uint8_t m_stepsSinceLastAvg;
 
--- the_timer/timer-code/src/Altimiter.cpp
+++ the_timer/timer-code/src/Altimiter.cpp
@@ -8,13 +8,85 @@ Altimeter::Altimeter() {
     for(int i = 0; i < m_numValuesToAvg; i++) {
         m_rollingVelocity[i] = 0;
     }
+
     //TODO: set up I2C here
+    // need to connect then read in factory calibration data to coeficcients array
+    // send reset command
+    // next send command to see each subsequent prom address and store value
+    m_i2c->begin();
+    m_i2c->beginTransmission(ADDRESS);
+    m_i2c->write(CMD_RESET);
+    m_i2c->endTransmission(true);
+    delay(3);
+
+    for(uint8_t i = 0; i < 8; i++) {
+        //get value from prom address
+            //this looks like: send command
+            //request bits
+            //read bits
+        m_i2c->beginTransmission(ADDRESS);
+        m_i2c->write(CMD_ADC_READ + i * 2);
+        m_i2c->endTransmission(true);
+
+        m_i2c->requestFrom(ADDRESS, 2);             // Request 2 bytes from chip
+        m_coefficients[i] = m_i2c->read() << 8;     // Store first byte of coefficient
+        m_coefficients[i] += m_i2c->read();         // Store 2nd byte of coefficient
+    }
+}
+
+uint32_t Altimeter::m_calculateAltitude() {
+    // read digital pressure and temperature data
+    // Get D1
+    m_i2c->beginTransmission(ADDRESS);
+    m_i2c->write(CMD_CONVERT_D1);
+    m_i2c->endTransmission(true);
+
+    delay(4); //TODO: is this enough delay?? use 1024 OSR instead please
+
+    m_i2c->requestFrom(ADDRESS, 3); // this uh, kind of sucks
+    m_D1 = m_i2c->read() << 16;
+    m_D1 += m_i2c->read() << 8;
+    m_D1 += m_i2c->read();
+
+    // Get D2
+    m_i2c->beginTransmission(ADDRESS);
+    m_i2c->write(CMD_CONVERT_D2);
+    m_i2c->endTransmission(true);
+
+    delay(4);
+
+    m_i2c->requestFrom(ADDRESS, 3);
+    m_D2 = m_i2c->read() << 16;
+    m_D2 += m_i2c->read() << 8;
+    m_D2 += m_i2c->read();
+
+    //cool what now
+    //we need to calculate all the bullshit
+
+    // Calculate temperature
+    int32_t dT = m_D2 - m_coefficients[4] * (2 << 8);
+    int32_t TEMP = 2000 + dT * (m_coefficients[5] >> 23);
+
+    // Calculate temperature compensated pressure
+    int64_t OFF = (m_coefficients[1] << 17) + ((m_coefficients[3] * dT) >> 6);
+    int64_t SENS = (m_coefficients[0] << 16) + ((m_coefficients[2] * dT) >> 7);
+    int32_t P = (m_D1 * (SENS >> 21) - OFF) >> 15;
+    // we probably don't need 2nd order temperature compensation right
+    // right guys haha
+    // TODO
+
+    // ok cool now we have to somehow convert pressure to altitude
+    // this sucks
+
+    //TODO: formula to convert pressure to altitude
+
+    return 1;
 }
 
 void Altimeter::update(const uint32_t &currentTimeMillis) {
-    float fakeAltitude = 7; //TODO: get altimeter reading here over I2C
+    float newAltitude = m_calculateAltitude();
     m_previousAltitude = m_currentAltitude;
-    m_currentAltitude = fakeAltitude - m_startingAltitude;
+    m_currentAltitude = newAltitude - m_startingAltitude;
 
     //ok we have altitude now we need to take a derivative
     // velocity = (altitude - previousAltitude) / timestep
@@ -59,6 +131,6 @@ float Altimeter::getAvgVelocity() {
 
 void Altimeter::setZero() {
     delay(10);
-    m_startingAltitude = 7;                 //TODO: ------------------------------ get altitude here over I2C
+    m_startingAltitude = m_calculateAltitude();
     delay(10);
 }
